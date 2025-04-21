997c6eece89a05ab342ccb16effb82f890932eaa
Emily Barbour <ebarbour@anduril.com>
2025-04-21 15:02:20 -0700

    update to new format

--- home/firefox.nix
+++ home/firefox.nix
@@ -3,7 +3,7 @@
     enable = true;
 
     profiles.default = {
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
         bitwarden
       ];
 
