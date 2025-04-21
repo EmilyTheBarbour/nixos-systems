ea079a28d8228aefaac9fbae363cf5180c907a26
Emily Barbour <ebarbour@anduril.com>
2025-04-21 14:57:34 -0700

    update firefox extensions

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,5 +1,14 @@
-{
+{pkgs, ...}: {
   programs.firefox = {
     enable = true;
+
+    extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      bitwarden
+    ];
+
+    settings = {
+      "extensions.autoDisableScopes" = 0;
+      "browser.toolbars.bookmarks.visibility" = "always";
+    };
   };
 }
