8cdc5d98f5e8b03d0c80f27126d88de4f5290350
Emily Barbour <ebarbour@anduril.com>
2025-04-24 11:38:33 -0700

    bump

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:EmilyTheBarbour/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nur.url = "github:nix-community/NUR";
