e229116eede755d556fed25663214db509821bd1
Emily Barbour <emilythebarbour@gmail.com>
2025-09-05 21:56:14 -0700

    work in progress

--- home/gaming/wallpaper-engine.nix
+++ home/gaming/wallpaper-engine.nix
@@ -1,15 +1,25 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    linux-wallpaperengine
-  ];
+{ pkgs, lib, config, ... }: {
+  options = {
+    wallpaper-id = lib.mkOption {
+      type = lib.types.int;
+      default = 123123123123;
+    };
+  };
 
-  # systemd.user.services.wallpaper-engine = {
-  #   Unit = {
-  #     Description = "Automatically run wallpaper-engine wallpaper of choice on log-in";
-  #   };
-  #   Install = {
-  #     WantedBy = [ "default.target" ];
-  #   };
-  #   Service = { ExecStart = "${pkgs.getExe pkgs.linux-wallpaperengine}" };
-  # };
+  config = {
+    home.packages = with pkgs; [
+      linux-wallpaperengine
+    ];
+
+    # systemd.user.services.wallpaper-engine = {
+    #   Unit = {
+    #     Description = "Automatically run wallpaper-engine wallpaper of choice on log-in";
+    #   };
+    #   Install = {
+    #     WantedBy = [ "default.target" ];
+    #   };
+    #   Service = { ExecStart = "${pkgs.getExe pkgs.linux-wallpaperengine} --silent ${config.wallpaper-id}" };
+    # };
+  };
+
 }
