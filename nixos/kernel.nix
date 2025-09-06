f36ce1036a7f29bd7cdb6ebe236db932734285f4
Emily Barbour <emilythebarbour@gmail.com>
2025-09-06 13:53:29 -0700

    work in progress

--- nixos/kernel.nix
+++ nixos/kernel.nix
@@ -1,3 +1,6 @@
 {pkgs, ...}: {
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  
+  # Only keep the last 5 configs present in the bootloader, no matter their age
+  boot.loader.systemd-boot.configurationLimit = 5;
 }
