199394d636e02941cdd890c937a1ae28392dcfe6
Emily Barbour <ebarbour@anduril.com>
2025-09-08 12:20:07 -0700

    actually make this configurable

--- nixos/machine/dell-precision-5690/camera.nix
+++ nixos/machine/dell-precision-5690/camera.nix
@@ -1,9 +1,10 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  config = {
+{ config
+, pkgs
+, lib
+, ...
+}:
+let cfg = config.machine.dell-precision-5690; in {
+  config = (lib.mkIf (cfg.enable-camera-config) lib.trace "enabling experimental 5690 camera support" {
     #TODO(Emily): Currently this is semi-working via pipewire. However, Video through pipewire is still rather new,
     # and as such many services don't have correct integrations, i.e zoom.
     #
@@ -12,10 +13,10 @@
 
     boot = {
       # Ensure the correct kernel module for the 5690 camera is loaded at boot time
-      kernelModules = ["intel_ipu6"];
+      kernelModules = [ "intel_ipu6" ];
 
       # Ensure we properly include the kernel module to interface with our 5690 camera
-      extraModulePackages = with config.boot.kernelPackages; [ipu6-drivers];
+      extraModulePackages = with config.boot.kernelPackages; [ ipu6-drivers ];
     };
 
     # TODO(emily): Make this a dev-shell for testing, required modules to use our
@@ -47,5 +48,5 @@
         ];
       };
     };
-  };
+  });
 }
