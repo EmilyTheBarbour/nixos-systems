f40a0f7031b56a07c77dc50a5ef01c52c1ba5b8a
Emily Barbour <ebarbour@anduril.com>
2025-09-09 10:44:35 -0700

    switch to production GPU drivers

--- nixos/machine/dell-precision-5690/hardware.nix
+++ nixos/machine/dell-precision-5690/hardware.nix
@@ -50,7 +50,7 @@
         powerManagement.finegrained = false;
         open = false;
         nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        package = config.boot.kernelPackages.nvidiaPackages.production;
       };
 
       enableAllFirmware = true;
