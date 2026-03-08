917934a6377790b1ae85b7abd4c40b0f3a135a19
Emily Barbour <emilythebarbour@gmail.com>
2026-03-08 08:25:36 -0700

    update

--- home/de/cosmic.nix
+++ home/de/cosmic.nix
@@ -3,6 +3,16 @@
     "widget.gtk.libadwaita-colors.enabled" = false;
   };
 
+  home.packages = with pkgs; [
+    cosmic-ext-applet-caffeine
+    cosmic-ext-applet-external-monitor-brightness
+    cosmic-ext-applet-minimon
+    cosmic-ext-applet-sysinfo
+    cosmic-ext-calculator
+    cosmic-ext-ctl
+    cosmic-ext-tweaks
+  ];
+
   home.file = 
     let folders = 
       let 
